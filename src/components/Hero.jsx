34c8852d9a38e5fce2054a9d26fefaeaaea93663
Dulran Hemjitha <dhemjitha@gmail.com>
2024-12-07 02:35:55 +0530

    fixed: hero button onClick

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { Button } from './ui/button';
+'use client'
+
+import React from "react";
+import { Button } from "./ui/button";
 
 function Hero() {
+  const scrollToAchievements = () => {
+    const achievementsSection = document.getElementById("achievements");
+    if (achievementsSection) {
+      achievementsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className="flex items-center justify-center  text-center px-6" style={{ height: "115vh" }}>
+    <div
+      className="flex items-center justify-center text-center px-6"
+      style={{ height: "115vh" }}
+    >
       <div>
         <p className="text-4xl md:text-5xl lg:text-8xl font-bold text-gray-300 mb-4 container">
           Boosting Business Growth with Smart Technology.
@@ -13,14 +25,21 @@ function Hero() {
         </p>
         <p className="text-lg md:text-xl lg:text-2xl text-gray-400 max-w-3xl mx-auto px-2 container">
           Tailored technologies designed to help your business grow.
-        </p><br />
+        </p>
+        <br />
         <div className="space-x-3">
-        <Button className="font-medium">Get Started</Button>
-        <Button variant="outline" className="font-bold">Discover More</Button>
+          <Button className="font-medium">Get Started</Button>
+          <Button
+            variant="outline"
+            className="font-bold"
+            onClick={scrollToAchievements}
+          >
+            Discover More
+          </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Hero;
