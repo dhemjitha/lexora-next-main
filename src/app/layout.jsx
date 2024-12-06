af72b19b65e3b21f55bae4e0d7ec0962b9b84e9b
Dulran Hemjitha <dhemjitha@gmail.com>
2024-12-06 19:53:25 +0530

    fixed: font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,5 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
-
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Synolux: Building with Bits | Synolux",
@@ -22,10 +9,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body 
-        className={`${geistSans.variable} ${geistMono.variable} antialiased` }
-      >
-        <Navigation/>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;700&display=swap"
+        />
+      </head>
+      <body className="antialiased">
+        <Navigation />
         {children}
       </body>
     </html>
