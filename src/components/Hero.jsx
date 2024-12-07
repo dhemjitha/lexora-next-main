05d4cc00fd09f101f525207c68e3765d71fb1025
Dulran Hemjitha <dhemjitha@gmail.com>
2024-12-07 22:31:33 +0530

    feat: gsap animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
-'use client'
+'use client';
 
-import React from "react";
+import React, { useContext, useRef } from "react";
 import { Button } from "./ui/button";
+import { useGSAP } from '@gsap/react';
+import { TransitionContext } from "./shared/TransitionContext";
+import gsap from "gsap";
 
 function Hero() {
   const scrollToAchievements = () => {
@@ -11,10 +14,29 @@ function Hero() {
     }
   };
 
+  const { timeline } = useContext(TransitionContext);
+  const container = useRef(null);
+
+  // GSAP Animation Hook
+  useGSAP(() => {
+    const targets = container.current.querySelectorAll("p, .space-x-3"); // Select p tags within the container
+    gsap.fromTo(
+      targets,
+      { y: 30, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.25, duration: 0.5 } // Added duration for smoother animation
+    );
+
+    // Optional timeline animation for the container
+    if (timeline) {
+      timeline.add(gsap.to(container.current, { opacity: 0 }));
+    }
+  }, { scope: container });
+
   return (
     <div
       className="flex items-center justify-center text-center px-6"
       style={{ height: "115vh" }}
+      ref={container}
     >
       <div>
         <p className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-gray-300 mb-4 container">
