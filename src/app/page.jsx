74b9582a9969e5452da66d7bbc3295da2559c86b
Dulran Hemjitha <dhemjitha@gmail.com>
2024-12-08 00:17:36 +0530

    fixed: remove additional marquee

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@ import Achievements from "@/components/Achivements";
 import FAQS from "@/components/Faq";
 import FeatureShowcase from "@/components/FeatureShowcase";
 import Hero from "@/components/Hero";
-import Marquee from "@/components/Marquee";
 import MarqueeComponent from "@/components/TechnologyMarquee";
 import ScrollAnimatedText from "@/components/Word";
 import Services from "@/components/Services";
