8658ca89363d5629264df121abd127789aa05aa5
Dulran Hemjitha <dhemjitha@gmail.com>
2025-01-23 22:01:26 +0530

    fixed: navigation link

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -18,7 +18,7 @@ const Navigation = () => {
                 <div className="container mx-auto px-6 py-3 flex items-center justify-between">
 
                     {/* Logo */}
-                    <a className="flex h-12 w-36 items-center rounded" href="/">
+                    <Link className="flex h-12 w-36 items-center rounded" href="/">
                         <Image
                             alt="SYNOLUX Logo"
                             loading="lazy"
@@ -28,7 +28,7 @@ const Navigation = () => {
                             srcSet="/assets/logo/logo.png 1x, /assets/logo/logo@2x.png 2x"
                             src="/assets/logo/logo.png"
                             style="color: transparent;" />
-                    </a>
+                    </Link>
 
                     {/* Navigation Links */}
                     <nav className="hidden md:flex items-center space-x-11 text-sm font-semibold">
